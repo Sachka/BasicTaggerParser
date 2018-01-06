ac56f5db5707a8694e84ab879777e28f06d4c336
timothee <timothee@chimpsky.home>
2018-01-06 20:15:50 +0100

    required outline for tagger

--- tagger.py
+++ tagger.py
@@ -1,15 +1,17 @@
+import corpus
+
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Embedding, LSTM, TimeDistributed, Bidirectional
 from keras.optimizers import Adam
 from keras.preprocessing.sequence import pad_sequences
 
-
+"""
 def read_conll_sentence(istream):
     x_seq = []
     y_seq = []
     line = istream.readline()
-    while line and not line.isspace():
+    while line and not line.isspace():s
         fields = line.split()
         x_seq.append(fields[1])
         y_seq.append(fields[3])
@@ -45,4 +47,15 @@ rev_x_codes.extend(list(x_set))
 rev_y_codes = list(y_set)
 x_codes = dict((x, idx) for idx, x in enumerate(rev_x_codes))
 y_codes = dict((y, idx) for idx, y in enumerate(rev_y_codes))
-print(y_codes)
+print(y_codes)"""
+
+class NNTagger(object) :
+	def __init__(self) :
+		pass
+	def train(self, corpus, **kwargs) :
+		pass
+	def tag(self, wordlist) :
+		pass
+	def predict(self, wordlist) :
+		pass
+
