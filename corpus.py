590737756402fa54b1abb11955343599c3472e3a
timothee <timothee@chimpsky.home>
2018-01-16 18:45:43 +0100

    flat extract func

--- corpus.py
+++ corpus.py
@@ -24,6 +24,10 @@ def split(filename, proportions=(("train", .8), ("dev", .1), ("test", .1)), rand
 def extract(corpus, columns=("token", "POS")) :
 	return [[[w[COLS[c]] for w in s ] for s in corpus] for c in columns]
 
+def flat_extract(corpus, columns=("token", "POS")) :
+		return [[w[COLS[c]] for w in s ] for s in corpus for c in columns]
+
 if __name__ == "__main__" :
 	corpus = load(split("sequoia-corpus.np_conll", randomize=True)[0])
 	extract(corpus)[1][0][0]
+	flat_extract(corpus)[1][0]
