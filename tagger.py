b7f6b1d8d2fb12775d02c3a21f145a1286a2b1ed
timothee <timothee@chimpsky.home>
2018-01-17 19:14:49 +0100

    tagger predict (no fix yet)

--- tagger.py
+++ tagger.py
@@ -33,9 +33,11 @@ class NNTagger(object) :
 		self.y_list = list({c for y in Y for c in y}) + ["__UNK__"]
 		self.x_codes = {x: idx for idx, x in enumerate(self.x_list)}
 		self.y_codes = {y: idx for idx, y in enumerate(self.y_list)}
+		self.reverse_x_codes = {i : x for i, x in enumerate(self.x_list)}
+		self.reverse_y_codes = {i : y for i, y in enumerate(self.y_list)}
 		Xcodes, Ycodes = self._encode(X, Y)
-		L=list(map(len, Ycodes))
-		self.mL = int(sum(L)/len(L))
+		L = list(map(len, Ycodes))
+		self.mL = max(L)
 		Xcodes = pad_sequences(Xcodes, maxlen=self.mL)
 		Ycodes = pad_sequences(Ycodes, maxlen=self.mL)
 		self.x_size = len(self.x_codes)
@@ -47,17 +49,14 @@ class NNTagger(object) :
 		self.model = Model(ipt, o)
 		if verbose : self.model.summary()
 		self.model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
-		self.model.fit(Xcodes, Ycodes, epochs=epochs, batch_size=batch_size)
+		self.model.fit(Xcodes, Ycodes, epochs=epochs, verbose=verbose, batch_size=batch_size)
+	def save(self) :
+		self.model.save("tagger.model.h5")
 
-	def tag(self, wordlist) :
-		data = numpy.zeros(self.MAX_SENTENCE_LEN, dtype='float32')
-		print(data.shape)
-		for i, word in enumerate(wordlist) :
-			data[i] = self.word_index[word] if word in self.word_index else len(self.word_index)
-		return [self.index_class[i] for i in self.model.predict(data)][:len(word_list)]
-
-	def predict(self, wordlist) :
-		return self.tag(wordlist)
+	def predict(self, sentences) :
+		X = [[(self.x_codes[word] if word in self.x_codes else self.x_codes["__UNK__"]) for word in sentence] for sentence in sentences]
+		predictions = self.model.predict(pad_sequences(X, maxlen=self.mL))
+		return [list(zip(sentences[i], map(lambda a : self.reverse_y_codes[numpy.argmax(a)], predictions[i]))) for i in range(len(sentences))]
 
 	def test(self, filename) :
 		X_test, Y_test = corpus.extract(corpus.load(filename))
@@ -67,6 +66,10 @@ class NNTagger(object) :
 		return self.model.evaluate(Xcodes_test, Ycodes_test, batch_size=64)
 
 if __name__ == "__main__" :
+	"""for embedding_size in range(20, 100, 10) :
+		for memory_size in range(20, 100, 10) :"""
 	tagger = NNTagger()
 	tagger.train("sequoia-corpus.np_conll.train", verbose=1)
 	print(tagger.test("sequoia-corpus.np_conll.test"))
+	#print(embedding_size, memory_size, score)
+	print(tagger.predict(corpus.extract(corpus.load("sequoia-corpus.np_conll.dev"))[0]))
